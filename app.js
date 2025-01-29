e9e90a31de52db7021e6d6f4cd4c98bf991602e7
Alexson Michel <alexsonmichel@gmail.com>
2025-01-29 20:59:53 -0300

    Corrigindo o template

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function exibirTextoNaTela(tag, texto) {
     responsiveVoice.speak(texto, 'Brazilian Portuguese Female',{rate:1.2});
 }
 function exibirMensagemInicial() {
-    exibirTextoNaTela('h1','Jogo do numero secreto');
+    exibirTextoNaTela('h1','Jogo do numero secreto.');
     exibirTextoNaTela('p','Escolha um número entre 1 e 10');
 }
 exibirMensagemInicial();
